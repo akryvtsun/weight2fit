315fdfac2dc5e6ffd14ba527f1bbddba3ff296cb
Andriy Kryvtsun <kontiky@gmail.com>
2014-10-06 20:45:50 -0400

    Initial FIT file creation

--- src/main/java/org/kryvtsun/weight2fit/App.java
+++ src/main/java/org/kryvtsun/weight2fit/App.java
@@ -10,5 +10,11 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         Mesg msg = Factory.createMesg(MesgNum.WEIGHT_SCALE);
+
+//        FileEncoder encoder = new FileEncoder(new File("test.fit"));
+//        WeightScaleMesg mesg = new WeightScaleMesg();
+//        mesg.setWeight(82f);
+//        encoder.write(mesg);
+//        encoder.close();
     }
 }
