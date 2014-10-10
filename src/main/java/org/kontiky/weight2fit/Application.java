ab6ae78798ea9ec873f17f5548859d04fb5a9b9b
Andriy Kryvtsun <kontiky@gmail.com>
2014-10-09 23:25:15 -0400

    BufferEncoder usage

--- src/main/java/org/kontiky/weight2fit/Application.java
+++ src/main/java/org/kontiky/weight2fit/Application.java
@@ -2,18 +2,24 @@ package org.kontiky.weight2fit;
 
 import com.garmin.fit.*;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Logger;
 
 /**
- * @see <a href="https://forums.garmin.com/showthread.php?24518-Import-of-Withings-wifi-scale-data-now-possible"/>Import of Withings wifi scale data now possible...</a>
- * @see <a href="http://jmfloreszazo.com/scale2fi">scale2fit</a>
- * @see <a href="https://github.com/marchibbins/fit-weight-scale">fit-weight-scale</a>
- * @see <a href="http://connect.garmin.com/health">garmin.com/health</a>
+ * https://forums.garmin.com/showthread.php?24518-Import-of-Withings-wifi-scale-data-now-possible
+ * http://jmfloreszazo.com/scale2fi
+ * https://github.com/marchibbins/fit-weight-scale
+ * http://connect.garmin.com/health
  */
 public class Application {
-    public static void main( String[] args ) {
-        FileEncoder fitFile = new FileEncoder();
-        fitFile.open(new java.io.File("test.fit"));
+    private static final Logger LOG = Logger.getLogger(Application.class.getName());
+
+    public static void main( String[] args ) throws IOException {
+        LOG.info("Starting coding...");
+
+        BufferEncoder encoder = new BufferEncoder();
 
         {
             FileIdMesg msg = new FileIdMesg();
@@ -23,31 +29,35 @@ public class Application {
             msg.setProduct(123);
             msg.setSerialNumber(4567L);
 
-            fitFile.write(msg);
+            encoder.write(msg);
         }
 
         {
             WeightScaleMesg msg = new WeightScaleMesg();
             // mandatory fields
             msg.setTimestamp(new DateTime(new Date()));
-            msg.setWeight(82.7f);               // Weight
+            msg.setWeight(82.7f);                   // Weight
             // other fields
-            msg.setPercentFat(40f);             // Body Fat
-            msg.setPercentHydration(57f);       // Body Water
+            msg.setPercentFat(40f);                 // Body Fat
+            msg.setPercentHydration(57f);           // Body Water
             msg.setVisceralFatMass(50f);
-            msg.setVisceralFatRating((short)1); // Visceral Fat
-            msg.setBoneMass(40f);               // Bone Mass
-            msg.setMuscleMass(30f);             // Muscle Mass
+            msg.setVisceralFatRating((short) 1);    // Visceral Fat
+            msg.setBoneMass(40f);                   // Bone Mass
+            msg.setMuscleMass(30f);                 // Muscle Mass
             msg.setBasalMet(3000f);
-            msg.setActiveMet(4000f);            // Daily Caloric Intake
-            msg.setPhysiqueRating((short)2);    // Physique Rating
-            msg.setMetabolicAge((short)35);     // Metabolic Age
+            msg.setActiveMet(4000f);                // Daily Caloric Intake
+            msg.setPhysiqueRating((short) 2);       // Physique Rating
+            msg.setMetabolicAge((short) 35);        // Metabolic Age
 
-            fitFile.write(msg);
+            encoder.write(msg);
         }
 
-        fitFile.close();
+        byte[] buffer = encoder.close();
 
-        System.out.println("FIT file was written");
+        try (FileOutputStream fos = new FileOutputStream(new java.io.File("test.fit"))) {
+            fos.write(buffer);
+        }
+
+        LOG.info("FIT file was written");
     }
 }
