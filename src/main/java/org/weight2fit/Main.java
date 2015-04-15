be8f276a87441435a4f5f18b2f507ac542cccc4a
Andriy Kryvtsun <andriy.kryvtsun@ubs.com>
2015-04-15 14:41:54 -0400

    usage Apache CLI

--- src/main/java/org/weight2fit/Main.java
+++ src/main/java/org/weight2fit/Main.java
@@ -1,11 +1,12 @@
 package org.weight2fit;
 
 import com.garmin.fit.Manufacturer;
-import org.weight2fit.infra.CommandLineProcessor;
+import org.apache.commons.cli.*;
 import org.weight2fit.infra.WeightScaleBuilder;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.logging.Logger;
 
 /**
@@ -18,23 +19,63 @@ import java.util.logging.Logger;
 public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class.getName());
 
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
     public static void main(String... args) throws Exception {
         LOG.info("Starting coding...");
 
-        CommandLineProcessor processor = new CommandLineProcessor();
-        processor.parse(args);
+        Option timestamp = OptionBuilder.withArgName("date")
+                .isRequired()
+                .hasArg()
+                .create("timestamp");
 
-        byte[] buffer = new WeightScaleBuilder()
-            .manufacturer(Manufacturer.TANITA)
-            .timestamp(processor.getTimestamp())
-            .weight(processor.getWeight())
-        .build();
+        Option weight = OptionBuilder.withArgName("weight")
+                .hasArg()
+                .create("weight");
 
-        File file = new File(processor.getOutFileName());
-        try (FileOutputStream fos = new FileOutputStream(file)) {
-            fos.write(buffer);
+        Option out = OptionBuilder.withArgName("file")
+                .isRequired()
+                .hasArg()
+                .create("out");
+
+        Options options = new Options();
+        options.addOption(timestamp);
+        options.addOption(weight);
+        options.addOption(out);
+
+        CommandLineParser parser = new BasicParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(options, args);
+
+            WeightScaleBuilder builder = new WeightScaleBuilder()
+                    .manufacturer(Manufacturer.TANITA);
+
+            if (line.hasOption("timestamp")) {
+                String value = line.getOptionValue("timestamp");
+                builder.timestamp(DATE_FORMAT.parse(value));
+            }
+
+            if (line.hasOption("weight")) {
+                String value = line.getOptionValue("weight");
+                builder.weight(Double.valueOf(value));
+            }
+
+            byte[] buffer = builder.build();
+
+            String outFile = line.getOptionValue("out");
+            File file = new File(outFile);
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                fos.write(buffer);
+            }
+
+            LOG.info("FIT file " + outFile + " was written");
         }
-
-        LOG.info("FIT file was written");
+        catch(ParseException exp) {
+            // oops, something went wrong
+            System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp( "weight2fit", options);
+        }
     }
 }
