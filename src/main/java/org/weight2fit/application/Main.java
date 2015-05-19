70bcb6b139499ecdab321cb3add73a8d2b1bde6f
englishman <kontiky@gmail.com>
2015-05-19 11:21:52 -0400

    reorganize classes for launching

--- src/main/java/org/weight2fit/application/Main.java
+++ src/main/java/org/weight2fit/application/Main.java
@@ -1,60 +1,37 @@
 package org.weight2fit.application;
 
-import org.weight2fit.application.cli.CmdLineParamsSupplier;
-import org.weight2fit.domain.FitParams;
-import org.weight2fit.domain.FitParamsConsumer;
-import org.weight2fit.infrastructure.FileParamsConsumer;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
 import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 /**
  * The main executable class.
- * Orchestrate all internal components.
+ * Initializes logging and starts application.
  *
  * @author Andriy Kryvtsun
  */
-// TODO add informative logging everywhere
-// TODO add internalization (in GUI)
 public class Main {
 
-    static {
-        try {
-            initLogging();
-        } catch (IOException e) { }
-    }
-
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-
     public static void main(String... args) {
-
         try {
-            CmdLineParamsSupplier supplier = new CmdLineParamsSupplier(args);
-
-            FitParams params = supplier.get();
-            File file = supplier.getFile();
-
-            FitParamsConsumer consumer = new FileParamsConsumer(file);
-            consumer.accept(params);
-
-            System.out.println("FIT file " + file + " was created");
+            initializeLogging();
+        } catch (IOException e) {
+            // avoid any error messages during init logging
         }
-        catch(Exception e) {
-            LOGGER.log(Level.SEVERE, "an exception was thrown", e);
 
-            System.exit(1);
-        }
+        Weight2Fit application = new Weight2Fit(args);
+        int result = application.execute();
+
+        System.exit(result);
     }
 
-    private static void initLogging() throws IOException {
+    // avoids logging output by default
+    private static void initializeLogging() throws IOException {
         String logFile = System.getProperty("java.util.logging.config.file");
-        if(logFile == null){
-            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("logging.properties");
-            LogManager.getLogManager().readConfiguration(inputStream);
+
+        if(logFile == null) {
+            InputStream propertiesStream = Main.class.getClassLoader().getResourceAsStream("logging.properties");
+            LogManager.getLogManager().readConfiguration(propertiesStream);
         }
     }
 }
---
+++ src/main/java/org/weight2fit/application/Weight2Fit.java
@@ -0,0 +1,52 @@
+package org.weight2fit.application;
+
+import org.weight2fit.application.cli.CmdLineParamsSupplier;
+import org.weight2fit.domain.FitParams;
+import org.weight2fit.domain.FitParamsConsumer;
+import org.weight2fit.infrastructure.FileParamsConsumer;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Weight2Fit application.
+ * Orchestrates all internal components.
+ *
+ * @author Andriy Kryvtsun
+ */
+// TODO add informative logging everywhere
+// TODO add internalization (in GUI)
+public class Weight2Fit {
+
+    private static final Logger LOG = Logger.getLogger(Weight2Fit.class.getName());
+
+    private final String[] args;
+
+    public Weight2Fit(String... args) {
+        this.args = args;
+    }
+
+    public int execute() {
+        int result = 0;
+
+        try {
+            CmdLineParamsSupplier supplier = new CmdLineParamsSupplier(args);
+
+            FitParams params = supplier.get();
+            File file = supplier.getFile();
+
+            FitParamsConsumer consumer = new FileParamsConsumer(file);
+            consumer.accept(params);
+
+            System.out.println("FIT file '" + file + "' was created");
+        }
+        catch(Exception e) {
+            LOG.log(Level.SEVERE, "an exception was thrown", e);
+
+            result = 1;
+        }
+
+        return result;
+    }
+}
