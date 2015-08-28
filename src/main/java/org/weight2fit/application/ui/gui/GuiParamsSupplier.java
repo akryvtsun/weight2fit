1f0319ab24c6f65ce67d722c553b268394b6c79d
englishman <kontiky@gmail.com>
2015-08-28 17:52:03 -0400

    fix DCI type

--- src/main/java/org/weight2fit/application/ui/gui/GuiParamsSupplier.java
+++ src/main/java/org/weight2fit/application/ui/gui/GuiParamsSupplier.java
@@ -103,7 +103,7 @@ class GuiParamsSupplier extends AbstractUiFitParamsSupplier implements FitParams
         physiqueRating = createField(group, "Physique &Rating:", null, FieldVerifiers.INTEGER);
         boneMass = createField(group, "B&one Mass:", "kg", FieldVerifiers.DOUBLE);
         metabolicAge = createField(group, "Metabolic &Age:", "years", FieldVerifiers.INTEGER);
-        dci = createField(group, "&DCI:", "C", FieldVerifiers.INTEGER);
+        dci = createField(group, "&DCI:", "C", FieldVerifiers.DOUBLE);
 
         return group;
     }
@@ -206,7 +206,7 @@ class GuiParamsSupplier extends AbstractUiFitParamsSupplier implements FitParams
             obtainIntParam(physiqueRating, FitFields.PHYSIQUE_RATING);
             obtainDoubleParam(boneMass, FitFields.BONE_MASS);
             obtainIntParam(metabolicAge, FitFields.METABOLIC_AGE);
-            obtainIntParam(dci, FitFields.DCI);
+            obtainDoubleParam(dci, FitFields.DCI);
         }
         catch (Exception e) {
             throw new FitException("Error during FitParams creation", e);
